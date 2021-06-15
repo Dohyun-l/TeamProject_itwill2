47f95934aa90bd3cd37c26003dcfdab5bb7d7b1a
Koo <42939097+rnrudxo2872@users.noreply.github.com>
2021-06-15 23:33:05 +0900

    테이블 컬럼 순서 변경

--- deco_user_table.sql
+++ deco_user_table.sql
@@ -24,24 +24,24 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `user_num` int(11) NOT NULL AUTO_INCREMENT COMMENT 'USER_NUM',
-  `addr` varchar(45) NOT NULL COMMENT 'ADDR',
+  `email` varchar(45) NOT NULL COMMENT 'EMAIL(UK)',
+  `pw` varchar(45) NOT NULL COMMENT 'PW',
+  `pre_pw` varchar(45) DEFAULT NULL,
   `name` varchar(45) NOT NULL COMMENT 'NAME',
   `nickname` varchar(45) NOT NULL COMMENT 'NICKNAME',
+  `addr` varchar(45) NOT NULL COMMENT 'ADDR',
+  `phone` varchar(45) NOT NULL COMMENT 'PHONE',
   `major` varchar(45) NOT NULL COMMENT 'MAJOR',
   `inter` varchar(45) DEFAULT NULL COMMENT 'INTER',
-  `pw` varchar(45) NOT NULL COMMENT 'PW',
   `create_at` date NOT NULL COMMENT 'CreateAt',
   `last_login` date NOT NULL COMMENT 'LASTLOGIN',
-  `email` varchar(45) NOT NULL COMMENT 'EMAIL(UK)',
-  `phone` varchar(45) NOT NULL COMMENT 'PHONE',
+  `change_pw` date DEFAULT NULL,
   `private_user` int(11) NOT NULL COMMENT 'private_user',
   `point` int(11) DEFAULT NULL COMMENT 'Point',
-  `change_pw` date DEFAULT NULL,
-  `pre_pw` varchar(45) DEFAULT NULL,
   `admin_auth` int(11) DEFAULT NULL COMMENT 'admin',
   PRIMARY KEY (`user_num`),
   UNIQUE KEY `user_email_uk` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,8 +50,13 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'rnrudxo','123','2020-04-12','koo','koo','1234','124','koo','213','2020-04-12','2020-04-12','2020-04-12',1,123,1),(2,'2020-04-12','2020-04-12','2020-04-12','2020-04-12','2020-04-12','2020-04-12','2020-04-12','2020-04-12','2020-04-12','2020-04-12','2020-04-12','2020-04-12',2,412,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'deco'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -62,4 +67,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-15 18:03:26
+-- Dump completed on 2021-06-15 23:32:23
