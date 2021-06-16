c2485cc662bec7acbd613129c1529a1b4fd65907
ITWILL <ITWILL@DESKTOP-875I1EB>
2021-06-16 16:44:13 +0900

    ShareController 제작

---
+++ src/com/deco/share/ShareController.java
@@ -0,0 +1,42 @@
+package com.deco.share;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.deco.Controller;
+
+@WebServlet("/users/*")
+public class ShareController extends Controller{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		setInit(req, res);
+		keyword = getKeyword(req,6);
+		
+		
+		
+		if(forward != null){
+			if(forward.isRedirect()){
+				res.sendRedirect(forward.getURL());
+				System.out.println("C : sendRedirect() 방식, " + forward.getURL() + "페이지 이동");
+			}else{ //false
+				System.out.println(forward.getURL());
+				RequestDispatcher dis = req.getRequestDispatcher(forward.getURL());
+				
+				dis.forward(req, res);
+				System.out.println("C : forward() 방식, " + forward.getURL());
+			}
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	
+	}
+	
+}
