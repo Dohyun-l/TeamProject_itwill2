8b4fa5b3cad37f6b12d0817ee3d80385c96b1ad6
ITWILL <ITWILL@DESKTOP-875I1EB>
2021-06-16 17:35:07 +0900

    shareController update

--- src/com/deco/share/ShareController.java
+++ src/com/deco/share/ShareController.java
@@ -8,16 +8,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.deco.ActionForward;
 import com.deco.Controller;
 
-@WebServlet("/users/*")
+@WebServlet("/shareInfo/*")
 public class ShareController extends Controller{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		setInit(req, res);
-		keyword = getKeyword(req,6);
+		keyword = getKeyword(req,10);
 		
+		
+		
+		
+		if (keyword.equals("/writeShare")){
+			System.out.println("C : /writeShare 호출");
+			forward = new ActionForward("../share/writeShare.jsp", false);
+			
+		}
 		
 		
 		if(forward != null){
